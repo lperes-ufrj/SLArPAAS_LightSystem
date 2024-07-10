65081ac012ca0fea433757b063f9654f573d4404
leoperes-ufrj <leoperes.ufrj@gmail.com>
2024-07-09 21:35:06 -0700

    updating code

--- KeithleyPowerSupply/TestMeasurement.py
+++ KeithleyPowerSupply/TestMeasurement.py
@@ -12,7 +12,7 @@ from time import sleep
 
 
 data_points = 50 
-voltage_probe = np.linspace(-52,-60,data_points)
+
 
 from pymeasure.adapters import VISAAdapter
 
@@ -24,15 +24,29 @@ keithley.source_voltage_range = -60  # Sets the source voltage range to -55 V
 keithley.compliance_current = 1e-4        # Sets the compliance current to 100 mu A.
 keithley.source_voltage = 0             # Sets the source voltage to 0 Volts.
 keithley.enable_source()                # Enables the source output
-
 keithley.measure_current()              # Sets up to measure voltage
 
+
+# Allocate arrays to store the measurement results
+voltage_probe = np.linspace(-52,-60,data_points)
+currents = np.zeros_like(voltage_probe)
+currents_stds = np.zeros_like(currents)
 
 for i in range(data_points):
 
     keithley.ramp_to_voltage(voltage_probe[i])  # Ramps the current to 5 mA
-    sleep(1.0)
-    print(keithley.current)                 # Prints the voltage in Volts
+    sleep(.5)
+    currents[i] = keithley.current  # Save current
+    currents_stds[i] = keithley.std_current # Save current std
+    sleep(.5)
+
+# Save the data columns in a CSV file
+data = pd.DataFrame({
+    'Current (A)': currents,
+    'Voltage (V)': voltages,
+    'Current Std (A)': currents_stds,
+})
+data.to_csv('IV_Curve.csv')
 
 
 keithley.shutdown()                     # Ramps the current to 0 mA and disables output
