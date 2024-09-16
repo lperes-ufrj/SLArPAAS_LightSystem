4d6c83258b44145a5b57e0afcac50a467b71b70f
leoperes-ufrj <leoperes.ufrj@gmail.com>
2024-09-16 10:16:13 -0700

    measurements with radioactive source

--- FirstSLArPAAS_SiPMChannel_ColdTest/tar_runfiles.py
+++ FirstSLArPAAS_SiPMChannel_ColdTest/tar_runfiles.py
@@ -13,7 +13,7 @@ def compress_files_with_label(directory, label, zip_filename):
 
 # Example usage
 directory_to_search = './'
-label_to_search = '2pf_2p7kohm_noradiosource_*'  # e.g. 'data'
-output_zip = '2pf_2p7kohm_noradiosource.zip'
+label_to_search = 'fulltpc_55p6mVtrigger_2pf_2p7kohm_WithRadiosource_*'  # e.g. 'data'
+output_zip = 'fulltpc_55p6mVtrigger_2pf_2p7kohm_WithRadiosource.zip'
 
 compress_files_with_label(directory_to_search, label_to_search, output_zip)
